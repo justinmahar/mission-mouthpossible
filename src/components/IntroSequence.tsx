3ad51e90213875e76ccf5be744daaed9c4233076
Justin Mahar <contact@justinmahar.com>
2025-04-16 16:38:44 -0400

    Remove unused import

--- src/components/IntroSequence.tsx
+++ src/components/IntroSequence.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ASSETS } from "../constants/game";
 import "./IntroSequence.css";
 
 const plotScreens = [
